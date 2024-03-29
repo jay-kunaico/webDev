fdaafbacc406714de78d66ee8dda03a5987bedc5
Jay-Kunaico <jay.brass@kunaico.com>
2024-03-29 09:00:32 -0400

    Callbacks practice updates

--- JSHardParts/callbacks.js
+++ JSHardParts/callbacks.js
@@ -163,7 +163,6 @@ function union(arrays) {
     []
   );
 }
-ddc;
 console.log(
   'Challenge 8 ',
   union([
@@ -173,3 +172,107 @@ console.log(
   ])
 );
 // should log: [5, 10, 15, 88, 1, 7, 100]
+
+// Challenge 9
+function objOfMatches(array1, array2, callback) {
+  const myObj = {};
+  // objOfMatches will test each element of the first array using the callback
+  array1.forEach((element, index) => {
+    const matched = callback(element);
+    //to see if the output matches the corresponding element (by index) of the second array
+    if (matched === array2[index]) {
+      myObj[element] = array2[index];
+    }
+  });
+
+  return myObj;
+}
+
+console.log(
+  objOfMatches(
+    ['hi', 'howdy', 'bye', 'later', 'hello'],
+    ['HI', 'Howdy', 'BYE', 'LATER', 'hello'],
+    function (str) {
+      return str.toUpperCase();
+    }
+  )
+);
+// should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
+
+// Challenge 10
+function multiMap(arrVals, arrCallbacks) {
+  const myObj = {};
+  //multiMap will return an object whose keys match the elements in the array of values.
+  arrVals.forEach((value) => {
+    //The corresponding values that are assigned to the keys will be arrays consisting of outputs from the array of callbacks, where the input to each callback is the key.
+    myObj[value] = arrCallbacks.map((callback) => callback(value));
+  });
+
+  return myObj;
+}
+
+console.log(
+  multiMap(
+    ['catfood', 'glue', 'beer'],
+    [
+      function (str) {
+        return str.toUpperCase();
+      },
+      function (str) {
+        return str[0].toUpperCase() + str.slice(1).toLowerCase();
+      },
+      function (str) {
+        return str + str;
+      },
+    ]
+  )
+);
+// should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+// Challenge 11
+function objectFilter(obj, callback) {
+  //objectFilter will return a new object.
+  const myObj = {};
+  //The new object will contain only the properties from the input object such that the property's value is equal to the property's key passed into the callback.
+  for (const key in obj) {
+    // check the value is === to the key passed into the callback so pass the key to the callback
+    if (callback(key) === obj[key]) {
+      // add that to the returnable obj
+      myObj[key] = obj[key];
+    }
+  }
+
+  return myObj;
+}
+
+const cities = {
+  London: 'LONDON',
+  LA: 'Los Angeles',
+  Paris: 'PARIS',
+};
+console.log(objectFilter(cities, (city) => city.toUpperCase()));
+// Should log { London: 'LONDON', Paris: 'PARIS'}
+
+// Challenge 12
+function majority(array, callback) {
+  let majorityIsTrue = false;
+  let numberTrue = 0;
+  let numberFalse = 0;
+  // majority will iterate through the array and perform the callback on each element
+  array.forEach((element) => {
+    //until it can be determined if the majority of the return values from the callback are true.
+    if (callback(element)) {
+      numberTrue++;
+    } else {
+      numberFalse++;
+    }
+  });
+  return (majorityIsTrue = numberTrue > numberFalse);
+}
+
+// /*** Uncomment these to check your work! ***/
+const isOdd = function (num) {
+  return num % 2 === 1;
+};
+console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
