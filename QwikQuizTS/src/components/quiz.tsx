d5b82eb492a8b1ca38b01a6db017ce693b6a13a0
Jay-Kunaico <jay.brass@kunaico.com>
2024-05-05 10:29:58 -0400

    Update questions to display 1 at a time

--- QwikQuizTS/src/components/quiz.tsx
+++ QwikQuizTS/src/components/quiz.tsx
@@ -1,137 +1,123 @@
-import { component$ } from '@builder.io/qwik';
-import { useQuestionData } from '~/routes';
+import { component$, useSignal, $ } from "@builder.io/qwik";
+import { useQuestionData } from "~/routes";
 
 export default component$(() => {
   const questions = useQuestionData();
+  const currentIndex = useSignal<number>(0);
+  const question = questions.value[currentIndex.value];
+  const selectedAnswer = useSignal<string | null>(null);
+
+  const nextQuestion$ = $(() => {
+    if (currentIndex.value < questions.value.length - 1) {
+      currentIndex.value++;
+      selectedAnswer.value = null; // Reset the selected answer
+    }
+  });
+
+  const handleAnswerChange$ = $((answer: string) => {
+    selectedAnswer.value = answer;
+  });
+
   return (
-    <div class="m-25">
+    <div class="m-5">
       <div
-        class="max-w-md mx-auto bg-white p-8 
-				rounded-md shadow-md"
+        class="mx-auto max-w-md rounded-md bg-white 
+				p-8 shadow-md"
         style="border: 1px solid black;"
       >
         <h1
-          class="text-3xl font-bold mb-6 
-				text-green-400 text-center 
-				text-success"
+          class="text-success mb-6 text-center 
+				text-3xl font-bold 
+				text-green-400"
         >
           Qwik TypeScript Quiz
         </h1>
         <form id="quizForm" class="space-y-4">
-          {questions &&
-            questions.value.map(
-              (question: {
-                id: number;
-                question: string | undefined;
-                answer1: string | undefined;
-                answer2: string | undefined;
-                answer3: string | undefined;
-                answer4: string | undefined;
-              }) => (
-                <div key={question.id} class="flex flex-col mb-8 py-4">
-                  <label for="q1" class="text-lg text-gray-800 mb-2">
-                    {question.id}. {question.question}
-                  </label>
-                  <div class="flex items-center space-x-4">
-                    <input
-                      type="radio"
-                      id="q1a"
-                      name="q1"
-                      value="a"
-                      class="mr-2"
-                      required
-                    />
-                    <label for="q1a" class="text-gray-700">
-                      a) {question.answer1}
+          <div>
+            {questions.value.length > 0 &&
+              currentIndex.value < questions.value.length && (
+                <div>
+                  <div>{questions.value[currentIndex.value].question}</div>
+                  <div key={question.id} class="mb-2 flex flex-col">
+                    <label for="q1" class="text-lg text-gray-800">
+                      {question.id}. {question.question}
                     </label>
+                    {["a", "b", "c", "d"].map((key, index) => {
+                      const answerKey = `answer${index + 1}`;
+                      return (
+                        <div key={key}>
+                          <input
+                            type="radio"
+                            id={`q${questions.value[currentIndex.value].id}${key}`}
+                            name={`q${questions.value[currentIndex.value].id}`}
+                            value={key}
+                            checked={selectedAnswer.value === key}
+                            onChange$={() => handleAnswerChange$(key)}
+                            required
+                          />
+                          <label for="q1a" class="text-gray-700">
+                            {" "}
+                            {
+                              questions.value[currentIndex.value][
+                                answerKey
+                              ] as string
+                            }
+                          </label>
+                        </div>
+                      );
+                    })}
                   </div>
-                  <div class="flex items-center space-x-4">
-                    <input
-                      type="radio"
-                      id="q1b"
-                      name="q1"
-                      value="b"
-                      class="mr-2"
-                      required
-                    />
-                    <label for="q1b" class="text-gray-700">
-                      b){question.answer2}
-                    </label>
-                  </div>
-                  <div class="flex items-center space-x-4">
-                    <input
-                      type="radio"
-                      id="q1c"
-                      name="q1"
-                      value="c"
-                      class="mr-2"
-                      required
-                    />
-                    <label for="q1c" class="text-gray-700">
-                      c) {question.answer3}
-                    </label>
-                  </div>
-                  <div class="flex items-center space-x-4">
-                    <input
-                      type="radio"
-                      id="q1d"
-                      name="q1"
-                      value="d"
-                      class="mr-2"
-                      required
-                    />
-                    <label for="q1d" class="text-gray-700">
-                      d) {question.answer4}
-                    </label>
-                  </div>{' '}
-                  <hr />
                 </div>
-              )
-            )}
-          <div class="flex justify-between">
-            <button
-              type="button"
-              class="bg-blue-500 hover:bg-blue-600 
-						text-white px-2 py-1 rounded-md"
-            >
-              Previous
-            </button>
-            <button
-              type="button"
-              class="bg-blue-500 hover:bg-blue-600 
-							text-white px-4 py-1 rounded-md"
-            >
-              Next
-            </button>
+              )}
           </div>
-          <hr />
 
+          <div class="flex justify-between">
+            {currentIndex.value > 0 && (
+              <button
+                onClick$={() => (currentIndex.value = currentIndex.value - 1)}
+                type="button"
+                class="rounded-md bg-blue-500 
+						px-2 py-1 text-white hover:bg-blue-600"
+              >
+                Previous
+              </button>
+            )}
+            {currentIndex.value < questions.value.length - 1 && (
+              <button
+                onClick$={nextQuestion$}
+                disabled={!selectedAnswer.value}
+                type="button"
+                class="rounded-md bg-blue-500 
+							px-4 py-1 text-white hover:bg-blue-600 disabled:bg-gray-400"
+              >
+                Next
+              </button>
+            )}
+          </div>
           <button
             type="button"
-            class="bg-green-500 text-white px-4 py-2 
-					rounded-md mt-8"
+            class="mt-8 rounded-md bg-green-500 px-4 
+					py-2 text-white"
           >
             Submit
           </button>
-
           <button
             type="button"
-            class="bg-red-500 text-white px-3 py-2 rounded-md mt-4"
+            class="mt-4 rounded-md bg-red-500 px-3 py-2 text-white"
           >
             Restart Quiz
           </button>
-
           <div id="result" class="mt-8 hidden">
-            <h2 class="text-2xl font-bold mb-4 text-center">
+            <h2 class="mb-4 text-center text-2xl font-bold">
               ???? Quiz Result
             </h2>
             <p
               id="score"
-              class="text-lg 
-								font-semibold mb-2 
-								text-center"
+              class="mb-2 
+								text-center text-lg 
+								font-semibold"
             ></p>
-            <p id="feedback" class="text-gray-700 text-center"></p>
+            <p id="feedback" class="text-center text-gray-700"></p>
           </div>
         </form>
       </div>
